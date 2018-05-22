17ba835abfa3ba6fa7f7a122207be1ea81af365a
DaveyRobinson <Robinson003@live.missouristate.edu>
2018-05-22 19:25:42 -0400

    Initial commit

---
+++ ben.js
@@ -0,0 +1,8 @@
+ $(document).ready(function() {
+   $("#adjectives").addClass("text-center");
+   $(".col-md-1").addClass("col-xs-1");
+   $(".container").css("background-color","silver").css("border-radius","10px")
+   $("#accomplishments").addClass("text-left");
+   $("p").css("width", "50%").css("display", "inline-block");
+   $("#quote").css("padding-top","30px");
+})
